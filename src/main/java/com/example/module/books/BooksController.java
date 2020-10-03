bdbd66145438ab8a8034c4a744e16bd2f4fbf793
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-03 14:57:13 +0200

    BooksController

--- src/main/java/com/example/module/books/BooksController.java
+++ src/main/java/com/example/module/books/BooksController.java
@@ -1,9 +1,7 @@
 package com.example.module.books;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,15 @@ public class BooksController {
     @GetMapping("/rest/books")
     public List<BooksEntity> getBooks() {
         return booksRepository.findAll();
+    }
+
+    @PostMapping("/rest/books")
+    public BooksEntity newBooks(@RequestBody BooksEntity booksEntity) {
+        return booksRepository.saveAndFlush(booksEntity);
+    }
+
+    @DeleteMapping("/rest/books/{id}")
+    public void delete(@PathVariable Long id) {
+        booksRepository.deleteById(id);
     }
 }
