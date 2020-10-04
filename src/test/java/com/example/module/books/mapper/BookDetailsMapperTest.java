7ce46dd3283fae6129a43345b03a1b6d76d3f0f7
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-04 12:29:42 +0200

    BookDetailsMapperTest

---
+++ src/test/java/com/example/module/books/mapper/BookDetailsMapperTest.java
@@ -0,0 +1,31 @@
+package com.example.module.books.mapper;
+
+import com.example.module.books.dto.BookDetailsDto;
+import com.example.module.books.entity.BooksDetailsEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookDetailsMapperTest {
+
+    @InjectMocks
+    private BookDetailsMapper bookDetailsMapper;
+
+    @Test
+    public void shouldReturnNotNullObject() {
+        //given
+        BooksDetailsEntity details = null;
+        //when
+        BookDetailsDto dto = bookDetailsMapper.map(details);
+        //then
+        assertThat(dto).isNotNull();
+    }
+
+
+}
