8993649c8a0318c54ed0118b568b6dfa32a91ab8
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-04 11:40:31 +0200

    BookD~etailsDto

--- src/main/java/com/example/module/books/dto/BookDetailsDto.java
+++ src/main/java/com/example/module/books/dto/BookDetailsDto.java
@@ -1,7 +1,24 @@
 package com.example.module.books.dto;
 
 public class BookDetailsDto {
-    private String cos;
-    private String cos1;
-    private String cos2;
+    private String isbn;
+    private String lang;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public BookDetailsDto setIsbn(String isbn) {
+        this.isbn = isbn;
+        return this;
+    }
+
+    public BookDetailsDto setLang(String lang) {
+        this.lang = lang;
+        return this;
+    }
 }
