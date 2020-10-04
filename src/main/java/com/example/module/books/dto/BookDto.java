f7e898e8fd683b4a3c277a2b0b2d90a53e72fb96
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-04 11:48:22 +0200

    BookDto

--- src/main/java/com/example/module/books/dto/BookDto.java
+++ src/main/java/com/example/module/books/dto/BookDto.java
@@ -3,6 +3,7 @@ package com.example.module.books.dto;
 public class BookDto {
     private String author;
     private String title;
+    private BookDetailsDto details;
 
     public String getAuthor() {
         return author;
@@ -12,6 +13,10 @@ public class BookDto {
         return title;
     }
 
+    public BookDetailsDto getDetails() {
+        return details;
+    }
+
     public BookDto setAuthor(String author) {
         this.author = author;
         return this;
@@ -19,6 +24,11 @@ public class BookDto {
 
     public BookDto setTitle(String title) {
         this.title = title;
+        return this;
+    }
+
+    public BookDto setDetails(BookDetailsDto details) {
+        this.details = details;
         return this;
     }
 }
