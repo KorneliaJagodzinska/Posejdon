126e60232acd4a7a95a513b84b32192d3ebcc01e
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-04 11:53:03 +0200

    BookDetailsMapper

--- src/main/java/com/example/module/books/mapper/BookDetailsMapper.java
+++ src/main/java/com/example/module/books/mapper/BookDetailsMapper.java
@@ -1,4 +1,15 @@
 package com.example.module.books.mapper;
 
+import com.example.module.books.dto.BookDetailsDto;
+import com.example.module.books.entity.BooksDetailsEntity;
+
 public class BookDetailsMapper {
+    public static BookDetailsDto map(BooksDetailsEntity details) {
+        if (details == null) {
+            return new BookDetailsDto();
+        }
+        return new BookDetailsDto()
+                .setIsbn(details.getIsbn())
+                .setLang(details.getLang());
+    }
 }
