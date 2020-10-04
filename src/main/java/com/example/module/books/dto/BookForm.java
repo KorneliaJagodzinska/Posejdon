f80316fdb6f086596c7b42140eec6af9115e9785
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-04 15:08:56 +0200

    BookFormMapper

--- src/main/java/com/example/module/books/dto/BookForm.java
+++ src/main/java/com/example/module/books/dto/BookForm.java
@@ -1,10 +1,13 @@
 package com.example.module.books.dto;
 
+import java.util.List;
+
 public class BookForm {
     private String author;
     private String title;
     private String isbn;
     private String lang;
+    private List<String> tags;
 
     public String getAuthor() {
         return author;
@@ -20,5 +23,9 @@ public class BookForm {
 
     public String getLang() {
         return lang;
+    }
+
+    public List<String> getTags() {
+        return tags;
     }
 }
--- src/main/java/com/example/module/books/mapper/BookFormMapper.java
+++ src/main/java/com/example/module/books/mapper/BookFormMapper.java
@@ -3,18 +3,35 @@ package com.example.module.books.mapper;
 import com.example.module.books.dto.BookForm;
 import com.example.module.books.entity.BooksDetailsEntity;
 import com.example.module.books.entity.BooksEntity;
+import com.example.module.books.entity.BooksTagsEntity;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BookFormMapper {
     public static BooksEntity map(BookForm form) {
-        return new BooksEntity()
-                .setTitle(form.getTitle())
+        BooksEntity entity = new BooksEntity();
+        entity.setTitle(form.getTitle())
                 .setAuthor(form.getAuthor())
-                .setDetails(getDetails(form));
+                .setDetails(getDetails(form))
+                .setTags(getTags(form, entity));
+        return entity;
+
     }
 
     private static BooksDetailsEntity getDetails(BookForm form) {
         return new BooksDetailsEntity()
                 .setIsbn(form.getIsbn())
                 .setLang(form.getLang());
+    }
+
+    private static Set<BooksTagsEntity> getTags(BookForm form, BooksEntity entity) {
+        return form
+                .getTags()
+                .stream()
+                .map(tag -> new BooksTagsEntity()
+                        .setValue(tag)
+                        .setBook(entity))
+                .collect(Collectors.toSet());
     }
 }
