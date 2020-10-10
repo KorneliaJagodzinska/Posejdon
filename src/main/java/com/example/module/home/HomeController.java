5a5d5b539aa5dcc2a7fe54f39d3a4242b61346be
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-10 10:28:04 +0200

    HomeController

--- src/main/java/com/example/module/home/HomeController.java
+++ src/main/java/com/example/module/home/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.module.home;
 
+import com.example.core.config.PosejdonConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private PosejdonConfig posejdonConfig;
+
     @Autowired
     private HomeService homeService;
+
+
     @RequestMapping({"/", "/index.html", "/index.php"})
-    public String getHome(Model model){
+    public String getHome(Model model) {
         model.addAttribute("greetings", homeService.getGreetings());
+        return "home";
+    }
+
+    @RequestMapping("/config")
+    public String getConfig(Model model) {
+        model.addAttribute("posejdonConfig", posejdonConfig);
         return "home";
     }
 }
